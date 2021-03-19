69fd81493059f17cb41ef6c1f029c9b795d414b4
boxbox557 <boxbox557@gmail.com>
2021-03-19 15:06:33 +0800

    add: new script to auto-replay youtube video

---
+++ youtube-autoreplay.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Youtube AutoReplay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically click Replay button after the video ends
+// @author       Cloude
+// @match        http://www.youtube.com/watch/*
+// @match        http://www.youtube.com/embed/*
+// @match        https://www.youtube.com/watch/*
+// @match        https://www.youtube.com/embed/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function observerCallback(mutationList, observer) {
+    for(let mutation of mutationList) {
+      if(mutation.attributeName === 'title' && mutation.target.title.toLowerCase() === "replay") {
+        console.log('[script] auto replay.')
+        mutation.target.click();      // auto click replay
+      }
+    }
+  }
+    
+  const observer = new MutationObserver(observerCallback);
+  const playBtn = document.querySelector('button.ytp-play-button');
+  observer.observe(playBtn, {attributes: true});
+
+  window.addEventListener('beforeunload', () => {
+    observer.disconnect();
+  });
+
+})();
