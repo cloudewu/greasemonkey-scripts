088e0e3926dbb6b4569fac7323a0fe43a0768282
boxbox557 <boxbox557@gmail.com>
2021-03-22 16:41:36 +0800

    [yt-autoreplay] feat: add simple setting button

--- youtube-autoreplay.js
+++ youtube-autoreplay.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube AutoReplay
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Automatically click Replay button after the video ends
 // @author       Cloude
 // @match        http://www.youtube.com/watch*
@@ -13,11 +13,23 @@
 (function() {
   'use strict';
 
+  function addAutoReplayBtn() {
+    let input = document.createElement('input', {is: 'btn-autoreplay'})
+    input.setAttribute('type', 'checkbox')
+    input.setAttribute('id', 'btn-autoreplay')
+    input.setAttribute('title', 'Auto Replay')
+    input.classList.add('ytp-button')
+
+    let container = document.querySelector('#movie_player .ytp-chrome-bottom .ytp-right-controls')
+    container.prepend(input)
+    
+    return input;
+  }
+  
   function observerCallback(mutationList, observer) {
     for(let mutation of mutationList) {
       if(mutation.attributeName === 'title' && mutation.target.title.toLowerCase() === "replay") {
         setTimeout(()=>{
-          console.log('[script] auto replay.')
           mutation.target.click(); // auto click replay
         }, 500);
       }
@@ -26,10 +38,19 @@
 
   const observer = new MutationObserver(observerCallback);
   const playBtn = document.querySelector('button.ytp-play-button');
-  observer.observe(playBtn, {attributes: true});
-
+  
+  let autoReplayBtn = addAutoReplayBtn();
+  autoReplayBtn.onchange = (e) => {
+    if(e.target.checked) {
+      observer.observe(playBtn, {attributes: true});
+      console.log('[script] autoreplay enabled')
+    } else {
+      observer.disconnect();
+      console.log('[script] autoreplay disabled')
+    }
+  }
+  
   window.addEventListener('beforeunload', () => {
     observer.disconnect();
   });
-
 })();
